c6bfe37a740162672e9eeb0ea40a298c38956998
Magdalena Siwek <magdalena.siwek@cfa.harvard.edu>
2026-04-29 18:11:34 -0400

    zenodo manifest now inside calypso/calypso/ for pip install

--- calypso/utils/constants.py
+++ calypso/utils/constants.py
@@ -8,15 +8,13 @@ norb = 10
 nbins = 100
 
 PKG_DIR = Path(__file__).resolve().parents[1]          # .../calypso/calypso
-REPO_ARTIFACTS_DIR = PKG_DIR.parent / ".calypso"     # pre-install only
-CACHE_DIR = Path.home() / ".cache" / "calypso"       # runtime downloads
+ASSETS_DIR = PKG_DIR / "assets"                        # bundled in the wheel
+CACHE_DIR = Path.home() / ".cache" / "calypso"         # runtime downloads
 
-BUNDLED_MANIFEST = REPO_ARTIFACTS_DIR / "assets" / "zenodo_manifest.json"
+BUNDLED_MANIFEST = ASSETS_DIR / "zenodo_manifest.json"
 CACHED_MANIFEST  = CACHE_DIR / "assets" / "zenodo_manifest.json"
 
 DEFAULT_ARTIFACTS_DIR = CACHE_DIR / "artifacts"
 DEFAULT_WEIGHTS_DIR = DEFAULT_ARTIFACTS_DIR
-BUNDLED_DATA_DIR = REPO_ARTIFACTS_DIR / "TS_data"           # pre-install only
-DEFAULT_DATA_DIR = CACHE_DIR / "TS_data"                    # runtime downloads
-DEFAULT_MDATA_DIR = PKG_DIR.parent / ".calypso" / "assets" / "models_meta.json"  # pre-install only
-DEFAULT_RUNTIME_META = REPO_ARTIFACTS_DIR / "assets" / "runtime.json"
+DEFAULT_DATA_DIR = CACHE_DIR / "TS_data"
+DEFAULT_RUNTIME_META = ASSETS_DIR / "runtime.json"
